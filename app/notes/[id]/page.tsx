2f120846f75a2547a9293077b402cb28e56682ef
Tkachov Oleksandr <smkys2003@gmail.com>
2026-08-06 09:36:50 +0300

    redo NoteDetails

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -4,7 +4,7 @@ import {
   QueryClient,
 } from "@tanstack/react-query";
 import { fetchNoteById } from "@/lib/api";
-import NoteDetailsClient from "../NoteDetails.client";
+import NoteDetailsClient from "./NoteDetails.client";
 
 export default async function NoteDetailsPage({
   params,
